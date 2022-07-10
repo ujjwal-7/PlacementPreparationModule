fe84041cbf239e6145bcd32ade1e692d0ac12318
ujjwal-7 <91021747+ujjwal-7@users.noreply.github.com>
2022-07-10 22:26:14 +0530

    Add files via upload

---
+++ Day 7/jumpGameVI.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    int maxResult(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+    
+        vector<int> dp(n);
+        deque<int> dq;
+        dq.push_back(n-1);
+        dp[n-1] = nums[n-1];
+        
+        for(int i = n - 2; i >= 0; i--) {
+            
+            if(i + k < dq.front())
+                dq.pop_front();
+            
+            dp[i] += nums[i] + dp[dq.front()];
+            while(!dq.empty() && dp[dq.back()] < dp[i])
+                dq.pop_back();
+            
+            dq.push_back(i);
+        }
+        
+        return dp[0];
+    }
+};
